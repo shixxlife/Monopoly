5cd25229d9955047dc681d1b6748bcfe1193c586
Aditi Shankar <150930722+shixxlife@users.noreply.github.com>
2025-03-11 22:41:21 +0530

    Add files via upload

---
+++ Client/src/players/Game2.js
@@ -0,0 +1,18 @@
+// src/Game.js
+import React from 'react';
+ // We'll create this file next
+
+function Game() {
+  return (
+    <div className="game-wrapper">
+      <iframe
+        src="MonopolyM_2.html"
+        title="Monopoly Game"
+        className="game-iframe"
+      />
+    </div>
+  );
+}
+
+export default Game;
+
---
+++ Client/src/players/Game3.js
@@ -0,0 +1,17 @@
+// src/Game.js
+import React from 'react';  // We'll create this file next
+
+function Game() {
+  return (
+    <div className="game-wrapper">
+      <iframe
+        src="MonopolyM_3.html"
+        title="Monopoly Game"
+        className="game-iframe"
+      />
+    </div>
+  );
+}
+
+export default Game;
+
